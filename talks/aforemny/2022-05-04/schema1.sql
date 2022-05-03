7d2dc3001bd4a1c4593bb95584ce908ca46febbf
Alexander Foremny <aforemny@posteo.de>
2022-05-03 23:49:29 +0200

    talks(postgREST): init schema1

---
+++ talks/aforemny/2022-05-04/schema1.sql
@@ -0,0 +1,13 @@
+create schema api;
+create table api.todos (
+  id serial primary key,
+  done boolean not null default false,
+  task text not null,
+  due timestamptz
+);
+insert into api.todos (task) values ('finish tutorial 0'), ('pat self on back');
+
+create role web_anon nologin;
+
+grant usage on schema api to web_anon;
+grant all on api.todos to web_anon;
